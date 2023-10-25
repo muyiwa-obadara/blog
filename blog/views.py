2bda88b3c906d12f270573effc1f26b8d718a095
Muyiwa Obadara <muyiwa.j.obadara@gmail.com>
2023-10-25 19:14:06 +0100

    initial views created

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.db.models.query import QuerySet
+from django.http import HttpResponse
 
-# Create your views here.
+from .models import Post
+
+def post_list(request) -> HttpResponse:
+    posts:QuerySet = Post.published.all()
+    return render(request, 'blog/post/list.html', {'posts': posts})
+
+def post_detail(request, id) -> HttpResponse:
+    post:Post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
+    return render(request, 'blog/post/detail.html', {'post': post})
