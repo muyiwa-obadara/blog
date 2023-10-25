3b0dd4f7281640660a6df74deaa1992fabd3799a
Muyiwa Obadara <muyiwa.j.obadara@gmail.com>
2023-10-25 18:56:35 +0100

    post initlal model designed

--- blog/models.py
+++ blog/models.py
@@ -1,12 +1,22 @@
 from django.db import models
+from django.db.models.query import QuerySet
 from django.utils import timezone
+from django.contrib.auth.models import User
+
+class PublishedManager(models.Manager):
+    def get_queryset(self) -> QuerySet:
+        return super().get_queryset()\
+            .filter(status=Post.Status.PUBLISHED)
 
 class Post(models.Model):
-
+    """
+    This is the post model.
+    """
     class Status(models.TextChoices):
         DRAFT = 'DF', 'Draft'
         PUBLISHED = 'PB', 'Published'
 
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_post')
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250)
     body = models.TextField()
@@ -14,6 +24,9 @@ class Post(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=2, choices=Status.choices, default=Status.DRAFT)
+
+    objects = models.Manager()
+    published = PublishedManager()
 
     class Meta:
         ordering = ['-publish']
