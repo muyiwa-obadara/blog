6b9b228c0f103df3b9dc0c4146d7678738766ad0
Muyiwa Obadara <muyiwa.j.obadara@gmail.com>
2023-11-04 19:52:57 +0100

    similar posts

--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,7 @@ from django.http import HttpResponse, HttpRequest
 from django.core.mail import send_mail
 from django.views.generic import ListView
 from django.views.decorators.http import require_POST
+from django.db.models import Count
 
 from taggit.models import Tag
 
@@ -40,9 +41,15 @@ def post_detail(request:HttpRequest, year:int, month:int, day:int, post:str) -> 
                                   publish__day=day)
     comments = post.comments.filter(active=True)
     form = CommentForm()
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids)\
+                                    .exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags'))\
+                                    .order_by('-same_tags', '-publish')
     return render(request, 'blog/post/detail.html', {'post': post,
                                                      'comments': comments,
-                                                     'form': form})
+                                                     'form': form,
+                                                     'similar_posts': similar_posts})
 
 def post_share(request, post_id):
     post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
