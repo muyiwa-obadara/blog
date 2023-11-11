2fb352a13371662e8dc1b57d583f3a8d9d64514b
Muyiwa Obadara <muyiwa.j.obadara@gmail.com>
2023-11-11 20:46:23 +0100

    sitemaps included

--- mysite/settings.py
+++ mysite/settings.py
@@ -18,6 +18,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+SITE_ID = 1
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -36,6 +37,8 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'taggit',
     'blog.apps.BlogConfig',
+    'django.contrib.sites',
+    'django.contrib.sitemaps',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
